1a1d3bbd6e525e243c8768f83c4d62496ca74ccc
Minato <bitcreditscc@gmail.com>
2016-06-08 22:14:25 +0200

    auxpow

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #include "addrman.h"
 #include "arith_uint256.h"
+#include "base58.h"
 #include "chainparams.h"
 #include "checkpoints.h"
 #include "checkqueue.h"
@@ -2727,6 +2728,12 @@ bool static ConnectTip(CValidationState& state, const CChainParams& chainparams,
         SyncWithWallets(tx, pindexNew, pblock);
     }
 
+	CBlock blockr;
+	string miner;
+	CTxDestination m;
+	ExtractDestination(pblock->vtx[0].vout[0].scriptPubKey, m);
+	miner = CBitcreditAddress(m).ToString().c_str();
+
 	sqlite3 *rawdb;
 	sqlite3_stmt *stmt;
 	char *zErrMsg = 0;
@@ -2734,67 +2741,62 @@ bool static ConnectTip(CValidationState& state, const CChainParams& chainparams,
 
 	std::map<std::string, int64_t>::iterator addrvalit;
 	std::map<std::string, int64_t> addressvalue = getbalances();
-	
-	if (fBaseNode){	
 
-		sqlite3_open((GetDataDir() / "ratings/rawdata.db").string().c_str(), &rawdb);
-		sqlite3_exec(rawdb, "PRAGMA synchronous = OFF", NULL, NULL, &zErrMsg);
-		sqlite3_exec(rawdb, "BEGIN TRANSACTION", NULL, NULL, &zErrMsg);
-	}
+
+	sqlite3_open((GetDataDir() / "ratings/rawdata.db").string().c_str(), &rawdb);
+	sqlite3_exec(rawdb, "PRAGMA synchronous = OFF", NULL, NULL, &zErrMsg);
+	sqlite3_exec(rawdb, "BEGIN TRANSACTION", NULL, NULL, &zErrMsg);
+
 	
 	BOOST_FOREACH(const CTransaction& tx, pblock->vtx){
 
-		
-		if (fBaseNode){		
-			char * insertquery = sqlite3_mprintf("insert into BLOCKS (ID, HASH, TIME, MINER) values (%lld,'%q',%lld,'%q')", pindexNew->nHeight, pblock->GetHash().ToString().c_str(), pblock->nTime, miner.c_str());
-			rc = sqlite3_exec(rawdb, insertquery, callback, 0, &zErrMsg);
-		}
+		char * insertquery = sqlite3_mprintf("insert into BLOCKS (ID, HASH, TIME, MINER) values (%lld,'%q',%lld,'%q')", pindexNew->nHeight, pblock->GetHash().ToString().c_str(), pblock->nTime, miner.c_str());
+		rc = sqlite3_exec(rawdb, insertquery, callback, 0, &zErrMsg);
+
 		for (unsigned int j = 0; j < tx.vout.size(); j++){
 			CTxDestination address;
 			ExtractDestination(tx.vout[j].scriptPubKey, address);
 			string receiveAddress = CBitcreditAddress(address).ToString().c_str();
 			int64_t theAmount = tx.vout[j].nValue;
 			addressvalue[receiveAddress] = addressvalue[receiveAddress] + theAmount;
-			if (fBaseNode){
-				char *sql = "select * from RAWDATA where ADDRESS = ?";
 
-				rc = sqlite3_prepare(rawdb, sql, strlen(sql), &stmt, 0);
-				sqlite3_bind_text(stmt, 1, receiveAddress.data(), receiveAddress.size(), 0);
-				if (sqlite3_step(stmt) == SQLITE_ROW){
+			char *sql = "select * from RAWDATA where ADDRESS = ?";
 
-					int64_t balance, txoutcount, totalout;
-					balance = sqlite3_column_int(stmt, 1);
-					txoutcount = sqlite3_column_int(stmt, 4);
-					totalout = sqlite3_column_int(stmt, 6);
-					sqlite3_finalize(stmt);
-					if (fDebug)LogPrintf("SQlite output record retrieved %s, %lld, %lld, %lld\n", receiveAddress, balance, txoutcount, totalout);
+			rc = sqlite3_prepare(rawdb, sql, strlen(sql), &stmt, 0);
+			sqlite3_bind_text(stmt, 1, receiveAddress.data(), receiveAddress.size(), 0);
+			if (sqlite3_step(stmt) == SQLITE_ROW){
 
-					char* updatequery = sqlite3_mprintf("update RAWDATA set BALANCE = %lld, TXOUTCOUNT =%lld, TOTALOUT= %lld where ADDRESS = '%q'", balance + theAmount, txoutcount + 1, totalout + theAmount, receiveAddress.c_str());
-					rc = sqlite3_exec(rawdb, updatequery, callback, 0, &zErrMsg);
+				int64_t balance, txoutcount, totalout;
+				balance = sqlite3_column_int(stmt, 1);
+				txoutcount = sqlite3_column_int(stmt, 4);
+				totalout = sqlite3_column_int(stmt, 6);
+				sqlite3_finalize(stmt);
+				if (fDebug)LogPrintf("SQlite output record retrieved %s, %lld, %lld, %lld\n", receiveAddress, balance, txoutcount, totalout);
 
-					if (rc != SQLITE_OK){
-						if (fDebug)LogPrintf("SQL update output error: %s\n", zErrMsg);
-						sqlite3_free(zErrMsg);
-					}
-					else{
-						if (fDebug)LogPrintf("update created successfully\n");
-					}
+				char* updatequery = sqlite3_mprintf("update RAWDATA set BALANCE = %lld, TXOUTCOUNT =%lld, TOTALOUT= %lld where ADDRESS = '%q'", balance + theAmount, txoutcount + 1, totalout + theAmount, receiveAddress.c_str());
+				rc = sqlite3_exec(rawdb, updatequery, callback, 0, &zErrMsg);
 
+				if (rc != SQLITE_OK){
+					if (fDebug)LogPrintf("SQL update output error: %s\n", zErrMsg);
+					sqlite3_free(zErrMsg);
 				}
 				else{
-					char * insertquery = sqlite3_mprintf("insert into RAWDATA (ADDRESS, BALANCE, FIRSTSEEN, TXOUTCOUNT, TOTALOUT) values ('%q',%lld,%lld,%lld,%lld)", receiveAddress.c_str(), theAmount, pblock->nTime, 1, theAmount);
-					rc = sqlite3_exec(rawdb, insertquery, callback, 0, &zErrMsg);
-
-					if (rc != SQLITE_OK){
-						if (fDebug)LogPrintf("SQL insert error: %s\n", zErrMsg);
-						sqlite3_free(zErrMsg);
-					}
-					else{
-						if (fDebug)LogPrintf("insert created successfully\n");
-					}
-					sqlite3_finalize(stmt);
+					if (fDebug)LogPrintf("update created successfully\n");
 				}
 			}
+			else{
+				char * insertquery = sqlite3_mprintf("insert into RAWDATA (ADDRESS, BALANCE, FIRSTSEEN, TXOUTCOUNT, TOTALOUT) values ('%q',%lld,%lld,%lld,%lld)", receiveAddress.c_str(), theAmount, pblock->nTime, 1, theAmount);
+				rc = sqlite3_exec(rawdb, insertquery, callback, 0, &zErrMsg);
+
+				if (rc != SQLITE_OK){
+					if (fDebug)LogPrintf("SQL insert error: %s\n", zErrMsg);
+					sqlite3_free(zErrMsg);
+				}
+				else{
+					if (fDebug)LogPrintf("insert created successfully\n");
+				}
+				sqlite3_finalize(stmt);
+			}			
 		}
 
 		for (size_t i = 0; i < tx.vin.size(); i++){
@@ -2811,7 +2813,7 @@ bool static ConnectTip(CValidationState& state, const CChainParams& chainparams,
 					CPubKey pubKey(vch);
 					prevoutHash = tx.vin[i].prevout.hash;
 					CTransaction txOfPrevOutput;
-					if (!GetTransaction(prevoutHash, txOfPrevOutput, blockHash, true)){
+					if (!GetTransaction(prevoutHash, txOfPrevOutput, Params().GetConsensus(), blockHash, true)){
 						if (fDebug)LogPrintf("AddrDB error Could not get transaction %s (output %d) referenced by input #%d of transaction %s in block %s\n"
 							, prevoutHash.ToString().c_str(), tx.vin[i].prevout.n, (int)i, tx.GetHash().ToString().c_str(), pblock->GetHash().ToString().c_str());
 						continue;
@@ -2830,46 +2832,43 @@ bool static ConnectTip(CValidationState& state, const CChainParams& chainparams,
 					spendAddress = CBitcreditAddress(addressRet).ToString().c_str();
 					theAmount = txOut.nValue;
 					addressvalue[spendAddress] = addressvalue[spendAddress] - theAmount;
-					if (fBaseNode){
-						const char *updatequery = "select * from RAWDATA where ADDRESS = ?";
+					const char *updatequery = "select * from RAWDATA where ADDRESS = ?";
 
-						rc = sqlite3_prepare(rawdb, updatequery, strlen(updatequery), &stmt, 0);
-						sqlite3_bind_text(stmt, 1, spendAddress.data(), spendAddress.size(), 0);
-						if (sqlite3_step(stmt) == SQLITE_ROW){
-							int64_t balance, txincount, totalin;
-							balance = sqlite3_column_int(stmt, 1);
-							txincount = sqlite3_column_int(stmt, 3);
-							totalin = sqlite3_column_int(stmt, 5);
+					rc = sqlite3_prepare(rawdb, updatequery, strlen(updatequery), &stmt, 0);
+					sqlite3_bind_text(stmt, 1, spendAddress.data(), spendAddress.size(), 0);
+					if (sqlite3_step(stmt) == SQLITE_ROW){
+						int64_t balance, txincount, totalin;
+						balance = sqlite3_column_int(stmt, 1);
+						txincount = sqlite3_column_int(stmt, 3);
+						totalin = sqlite3_column_int(stmt, 5);
 
-							if (fDebug)LogPrintf("SQlite input record retrieved %s, %lld, %lld, %lld \n", spendAddress, balance, txincount, totalin);
-							sqlite3_finalize(stmt);
-							char *updatequery = sqlite3_mprintf("update RAWDATA set BALANCE = %lld , TXINCOUNT =%lld,  TOTALIN= %lld where ADDRESS = '%q'", balance - theAmount, txincount + 1, totalin + theAmount, spendAddress.c_str());
+						if (fDebug)LogPrintf("SQlite input record retrieved %s, %lld, %lld, %lld \n", spendAddress, balance, txincount, totalin);
+						sqlite3_finalize(stmt);
+						char *updatequery = sqlite3_mprintf("update RAWDATA set BALANCE = %lld , TXINCOUNT =%lld,  TOTALIN= %lld where ADDRESS = '%q'", balance - theAmount, txincount + 1, totalin + theAmount, spendAddress.c_str());
+						rc = sqlite3_exec(rawdb, updatequery, callback, 0, &zErrMsg);
 
-							rc = sqlite3_exec(rawdb, updatequery, callback, 0, &zErrMsg);
-
-							if (rc != SQLITE_OK){
-								if (fDebug)LogPrintf("SQL update output error: %s\n", zErrMsg);
-								sqlite3_free(zErrMsg);
-							}
-							else{
-								if (fDebug)LogPrintf("update created successfully\n");
-							}
+						if (rc != SQLITE_OK){
+							if (fDebug)LogPrintf("SQL update output error: %s\n", zErrMsg);
+							sqlite3_free(zErrMsg);
+						}
+						else{
+							if (fDebug)LogPrintf("update created successfully\n");
 						}
 					}
 				}
 			}
 		}
 	}
-	if (fBaseNode){
-		sqlite3_exec(rawdb, "END TRANSACTION", NULL, NULL, &zErrMsg);
-		if (sqlite3_close(rawdb) != SQLITE_OK){
-			if (fDebug)LogPrintf("SQL unable to close database %s\n", sqlite3_errmsg(rawdb));
-			sqlite3_free(zErrMsg);
-		}
-		else{
-			if (fDebug)LogPrintf("database closed successfully\n");
-		}
+
+	sqlite3_exec(rawdb, "END TRANSACTION", NULL, NULL, &zErrMsg);
+	if (sqlite3_close(rawdb) != SQLITE_OK){
+		if (fDebug)LogPrintf("SQL unable to close database %s\n", sqlite3_errmsg(rawdb));
+		sqlite3_free(zErrMsg);
 	}
+	else{
+		if (fDebug)LogPrintf("database closed successfully\n");
+	}
+
 	ofstream addrdb;
 	addrdb.open((GetDataDir() / "ratings/balances.dat").string().c_str(), std::ofstream::trunc);
 
